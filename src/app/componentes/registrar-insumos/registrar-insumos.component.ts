39bbfeea04b41c55a749cbb62a6653cc7e336f30
Gabo990616 <tigab.990616@gmail.com>
2021-09-21 18:59:55 -0400

    codigo un poco mal

--- src/app/componentes/registrar-insumos/registrar-insumos.component.ts
+++ src/app/componentes/registrar-insumos/registrar-insumos.component.ts
@@ -7,23 +7,43 @@ import { InsumoServiceService } from 'src/app/servicios/insumo-service.service';
 @Component({
   selector: 'app-registrar-insumos',
   templateUrl: './registrar-insumos.component.html',
-  styleUrls: ['./registrar-insumos.component.css']
+  styleUrls: ['./registrar-insumos.component.css'],
 })
 export class RegistrarInsumosComponent implements OnInit {
-  insumo!:Insumo;
-  contratos!:Contrato[];
- 
-  constructor(private insumoservice:InsumoServiceService,private contratoservice:ContratoServiceService) { }
+  insumo!: Insumo;
+  contratos!: Contrato[];
+  contrato!: Contrato;
+  revisar: boolean = false;
+
+  constructor(
+    private insumoservice: InsumoServiceService,
+    private contratoservice: ContratoServiceService
+  ) {}
 
   ngOnInit(): void {
-    this.insumo=new Insumo;
-    this.contratos=this.contratoservice.get();
+    this.insumo = new Insumo();
+    this.contratos = this.contratoservice.get();
   }
-  add(){
-    alert("Se ha añadido un insumo");
-    this.insumoservice.post(this.insumo);
+
+  add() {
+    for (let i = 0; i < this.contratos.length; i++) {
+      this.contrato = this.contratos[i];
+      if (this.contrato.identificador === this.insumo.identificadorins) {
+        if (this.contrato.saldo > this.insumo.valorins) {
+          this.contrato.saldo -= this.insumo.valorins;
+          this.revisar = true;
+        }
+      }
+    }
+    if (this.revisar) {
+      alert('Se ha añadido un insumo');
+      this.insumoservice.post(this.insumo);
+      this.contratoservice.post(this.contrato);
+    }
+    else
+    alert('no es posiblerealizar esta accion')
   }
-ad(){
-  this.insumoservice.clear();
-}
+  ad() {
+    this.insumoservice.clear();
+  }
 }
